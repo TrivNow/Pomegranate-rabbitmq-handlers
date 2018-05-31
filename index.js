4a81a2193e9cb452de24eda898f6aab90f288902
Jim Bulkowski <jim.b@paperelectron.com>
2018-05-31 01:34:36 -0400

    Creates exclusive bound queues to ingest Exhange messages

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
  */
 
 'use strict';
-
+const Promise = require('bluebird')
 const path = require('path')
 const _ = require('lodash')
 const _fp = require('lodash/fp')
@@ -29,48 +29,75 @@ exports.metadata = {
 
 exports.plugin = {
   load: function(Injector, PluginFiles, Options, Logger, PluginContext, RabbitConnection) {
-
     PluginFiles.fileList()
-      .then(function(files){
-        return _fp.map(function(file){
+      .then(function(files) {
+        return _fp.map(function(file) {
           return Injector.inject(require(file.path));
         }, files)
       })
       .then(function(queues) {
         return RabbitConnection.createChannel()
-          .then(function(channel){
+          .then(function(channel) {
             PluginContext.channel = channel
-            _fp.each(function(queue){
-              channel.assertQueue(queue.name, {durable: true})
-            }, queues)
-            return channel
-          })
-          .then(function(channel){
-            return _fp.map(function(queue){
-              return function(){
-                Logger.log(`${queue.name} handler Starting.`)
-                return channel.consume(queue.name, queue.handler.bind(channel))
+
+            // Create the exchanges and queues based on exported object
+            return Promise.map(queues, (queue) => {
+              // Exchange exclusive queue
+              let ct = queue.type
+              let options = queue.options || {durable: true}
+
+              if(ct === 'direct' || ct === 'fanout' || ct === 'topic' || ct === 'headers') {
+                let pattern = queue.pattern || ''
+                Logger.log(`${ct} Exchange found.`)
+                return channel.assertExchange(queue.name, queue.type, options)
+                  .then((r) => {
+                    return channel.assertQueue('', {exclusive: true})
+                  })
+                  .then((q) => {
+                    // Bind the exclusive queue to the exchange.
+                    // Then return the exclusive exchange to capture the generated name.
+                    return channel.bindQueue(q.queue, queue.name, pattern).then(() => q)
+                  })
+                  .then((q) => {
+                    return function() {
+                      Logger.log(`${queue.name} handler Starting.`)
+                      return channel.consume(q.queue, queue.handler.bind(channel))
+                    }
+                  })
               }
 
-            }, queues)
-          })
+              // Actual queues
+              return channel.assertQueue(queue.name, options)
+                .then((q) => {
+                  return function() {
+                    Logger.log(`${queue.name} handler Starting.`)
+                    return channel.consume(q.queue, queue.handler.bind(channel))
+                  }
+                })
 
+            })
+
+          })
       })
-      .then(function(activeQueues){
+      .then(function(activeQueues) {
+        console.log(activeQueues)
         PluginContext.queues = activeQueues
         return true
+      })
+      .catch((err) => {
+        console.log(err)
       })
 
   },
   start: function(PluginContext, Logger) {
     _fp.each((queue) => {
-      if(_fp.isFunction(queue)){
+      if(_fp.isFunction(queue)) {
         queue()
       }
     }, PluginContext.queues)
   },
   stop: function(PluginContext, Logger) {
-    if(_fp.hasIn('channel.close', PluginContext) && _fp.isFunction(PluginContext.channel.close)){
+    if(_fp.hasIn('channel.close', PluginContext) && _fp.isFunction(PluginContext.channel.close)) {
       Logger.log('Closing Channel.')
       return PluginContext.channel.close().then(() => {
         Logger.log('Channel closed successfully')
